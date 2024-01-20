87e21a757c12c2274ac0d9dd864996b28cd79a2b
CHITHANURISOWMYA <sowmyagupta2000@gmail.com>
2024-01-20 12:20:19 +0530

    test

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -2,7 +2,7 @@
 const Navbar=()=>{
     return(
  <>
- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+ <nav class="navbar navbar-expand-lg navbar-light bg-light" >
   <a class="navbar-brand" href="#">Shop</a>
   <a class="navbar-brand" href="#">Read</a>
   <a class="navbar-brand" href="#">Store</a>
